623721479acb26feba0ab457a4026e643eaa4798
Henrik Lagrosen <henrikl@tuta.io>
2020-11-21 20:18:30 +0100

    updated port

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from staticServer import StaticServer
 
 
-def runServer(server_class=HTTPServer, handler_class=StaticServer, port=8000):
+def runServer(server_class=HTTPServer, handler_class=StaticServer, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print('Starting httpd on port {}'.format(port))
