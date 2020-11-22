12f5aa0fd922cab1f49c8677201d3cc7dc624d72
Henrik Lagrosen <henrikl@tuta.io>
2020-11-22 10:08:56 +0100

    cleaning

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import os
 from staticServer import StaticServer
-
 
 def runServer(server_class=HTTPServer, handler_class=StaticServer, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print('Starting httpd on port {}'.format(port))
     httpd.serve_forever()
-
 
 runServer()
