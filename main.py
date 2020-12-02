475aff0bac6765bf6e9a4db9f7396d7c2037e29e
Henrik Lagrosen <henrikl@tuta.io>
2020-12-02 16:14:42 +0100

    adds variable website address

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from staticServer import StaticServer
+import env
+
+address = "" if not hasattr(env, "websiteAddress") else getattr(env, "websiteAddress")
 
 def main():
     runServer()
 
-def runServer(server_class=HTTPServer, handler_class=StaticServer, port=8080):
-    server_address = ('', port)
+def runServer(server_class=HTTPServer, handler_class=StaticServer, port=8000):
+    server_address = (address, port)
     httpd = server_class(server_address, handler_class)
-    print('Starting httpd on port {}'.format(port))
+    print('Starting httpd on {0} port {1}'.format(address, port))
     httpd.serve_forever()
 
 main()
